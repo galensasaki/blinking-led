a210b4eb82a34ddcfcd3ec929282174826a6482b
Galen Sasaki <galens@hawaii.edu>
2025-10-31 14:58:00 -1000

    User inputs duration of blink

--- blinking_led.py
+++ blinking_led.py
@@ -2,10 +2,11 @@ from machine import Pin
 import time
 
 led = Pin("LED", Pin.OUT)
+duration = int(input("Enter duration: "))
 
 for i in range(5):
     led.value(1)
-    time.sleep(1)
+    time.sleep(duration)
     led.value(0)
     time.sleep(1)
     
